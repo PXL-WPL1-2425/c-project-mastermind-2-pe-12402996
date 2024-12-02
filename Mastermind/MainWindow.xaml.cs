07d0f1e33621d326a7c003835544f3c693503460
Berke Özdemir <12402996@student.pxl.be>
2024-12-02 14:14:26 +0100

    Mastermind-PE2-01

---
+++ Mastermind/MainWindow.xaml.cs
@@ -0,0 +1,418 @@
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Printing;
+using System.Reflection;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Net.WebRequestMethods;
+
+namespace C_mastermindSprint1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        List<string> colors = new List<string> { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+        private List<string> secretCode = new List<string>();
+        int guessAttempts = 0;
+        DateTime startedGuessTime;
+        int score = 100;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Weet je zeker dat je het spel wilt beëindigen?", 
+                "Afsluiten", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            } 
+            else
+            {
+                e.Cancel = false;
+
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var randomCode = new Random();
+
+            for (int i = 0; i < 4; i++)
+            {
+                secretCode.Add(colors[randomCode.Next(colors.Count)]);
+            }
+
+            generatedCodeTextBox.Text = $"{secretCode}".ToString();
+            this.Title = $"Poging: {guessAttempts}";
+            StartCountDown();
+            generatedCodeTextBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            // Deel 1. Bereken tijdsverschil
+            TimeSpan timeUsedToGuess = DateTime.Now - startedGuessTime;
+            // Deel 2. controle, if time > 10 seconds
+            if (timeUsedToGuess.TotalSeconds > 10)
+            {
+                StopCountDown();
+                startedGuessTime = DateTime.Now;
+            }
+            timerTextBox.Text = timeUsedToGuess.TotalSeconds.ToString("N0");
+        }
+        /// <summary>
+        /// Deze methode zorgt eerst en vooral ervoor dat de timer stopt en vervolgens de poging verhoogt. 
+        /// Nadien wordt er een messagebox getoond.
+        /// de titel van het venster wordt aangepast en de timer wordt opnieuw gestart.
+        /// </summary>
+        private void StopCountDown()
+        {
+            timer.Stop();
+            guessAttempts++;
+            MessageBox.Show("Je beurt is over, probeer opnieuw", "Je tijd is verstreken, To Slow.", MessageBoxButton.OK, MessageBoxImage.Warning);
+            this.Title = $"Poging: {guessAttempts}";
+            StartCountDown();
+        }
+        /// <summary>
+        /// Start de interval van de timer en zorgt ervoor dat de timer elke 100 milliseconden telt.
+        /// </summary>
+        private void StartCountDown()
+        {
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += Timer_Tick;
+            startedGuessTime = DateTime.Now;
+            timer.Start();
+        }
+
+
+        private void ComboBoxColour_SelectionChanged1(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender == comboBoxColour1 && comboBoxColour1.SelectedItem != null)
+            {
+                switch (comboBoxColour1.SelectedIndex)
+                {
+                    case 0:
+                        labelColorOne.Background = new SolidColorBrush(Colors.Red);
+                        break;
+                    case 1:
+                        labelColorOne.Background = new SolidColorBrush(Colors.Yellow);
+                        break;
+                    case 2:
+                        labelColorOne.Background = new SolidColorBrush(Colors.Orange);
+                        break;
+                    case 3:
+                        labelColorOne.Background = new SolidColorBrush(Colors.White);
+                        break;
+                    case 4:
+                        labelColorOne.Background = new SolidColorBrush(Colors.Green);
+                        break;
+                    case 5:
+                        labelColorOne.Background = new SolidColorBrush(Colors.Blue);
+                        break;
+                }
+            }
+            if (sender == comboBoxColour2 && comboBoxColour2.SelectedItem != null)
+            {
+                switch (comboBoxColour2.SelectedIndex)
+                {
+                    case 0:
+                        labelColorTwo.Background = new SolidColorBrush(Colors.Red);
+                        break;
+                    case 1:
+                        labelColorTwo.Background = new SolidColorBrush(Colors.Yellow);
+                        break;
+                    case 2:
+                        labelColorTwo.Background = new SolidColorBrush(Colors.Orange);
+                        break;
+                    case 3:
+                        labelColorTwo.Background = new SolidColorBrush(Colors.White);
+                        break;
+                    case 4:
+                        labelColorTwo.Background = new SolidColorBrush(Colors.Green);
+                        break;
+                    case 5:
+                        labelColorTwo.Background = new SolidColorBrush(Colors.Blue);
+                        break;
+                }
+            }
+            if (sender == comboBoxColour3 && comboBoxColour3.SelectedItem != null)
+            {
+                switch (comboBoxColour3.SelectedIndex)
+                {
+                    case 0:
+                        labelColorThree.Background = new SolidColorBrush(Colors.Red);
+                        break;
+                    case 1:
+                        labelColorThree.Background = new SolidColorBrush(Colors.Yellow);
+                        break;
+                    case 2:
+                        labelColorThree.Background = new SolidColorBrush(Colors.Orange);
+                        break;
+                    case 3:
+                        labelColorThree.Background = new SolidColorBrush(Colors.White);
+                        break;
+                    case 4:
+                        labelColorThree.Background = new SolidColorBrush(Colors.Green);
+                        break;
+                    case 5:
+                        labelColorThree.Background = new SolidColorBrush(Colors.Blue);
+                        break;
+
+                }
+            }
+            if (sender == comboBoxColour4 && comboBoxColour4.SelectedItem != null)
+            {
+                switch (comboBoxColour4.SelectedIndex)
+                {
+                    case 0:
+                        labelColorFour.Background = new SolidColorBrush(Colors.Red);
+                        break;
+                    case 1:
+                        labelColorFour.Background = new SolidColorBrush(Colors.Yellow);
+                        break;
+                    case 2:
+                        labelColorFour.Background = new SolidColorBrush(Colors.Orange);
+                        break;
+                    case 3:
+                        labelColorFour.Background = new SolidColorBrush(Colors.White);
+                        break;
+                    case 4:
+                        labelColorFour.Background = new SolidColorBrush(Colors.Green);
+                        break;
+                    case 5:
+                        labelColorFour.Background = new SolidColorBrush(Colors.Blue);
+                        break;
+                }
+
+            }
+        }
+       
+        private void checkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(comboBoxColour1.Text) ||
+              string.IsNullOrEmpty(comboBoxColour2.Text) ||
+              string.IsNullOrEmpty(comboBoxColour3.Text) ||
+              string.IsNullOrEmpty(comboBoxColour4.Text))
+            {
+                MessageBox.Show("Je moet een kleur kiezen", "Input Fout", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            guessAttempts++;
+            this.Title = $"Poging: " + guessAttempts;
+            UpdateTitle();
+
+            string checkColor1 = comboBoxColour1.Text;
+            string checkColor2 = comboBoxColour2.Text;
+            string checkColor3 = comboBoxColour3.Text;
+            string checkColor4 = comboBoxColour4.Text;
+            List<string> inputColor = new List<string> { checkColor1, checkColor2, checkColor3, checkColor4 };
+
+            if (guessAttempts >= 10)
+            {
+                timer.Stop();
+                MessageBoxResult result = MessageBox.Show($"Je hebt geen pogingen meer over. De correcte code was {string.Join(", ", secretCode)}. Wil je opnieuw spelen?"
+                    , "Game Over", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    ResetGame();
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+                return;
+            }
+
+            StartCountDown();
+
+
+            StackPanel colorPanel = new StackPanel { Orientation = Orientation.Horizontal};
+            for (int i = 0; i < inputColor.Count; i++)
+            {
+                Rectangle rect = new Rectangle
+                {
+                    Width = 190,
+                    Height = 40,
+                    Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString(inputColor[i]))
+                };
+
+                if (secretCode[i] == inputColor[i])
+                {
+                    rect.Stroke = Brushes.DarkRed;
+                    rect.StrokeThickness = 5;
+                }
+                else if (secretCode.Contains(inputColor[i]))
+                {
+                    rect.Stroke = Brushes.Wheat;
+                    rect.StrokeThickness = 5;
+                }
+                else
+                {
+                    rect.Stroke = Brushes.Transparent;
+                    rect.StrokeThickness = 5;
+                }
+
+                colorPanel.Children.Add(rect);
+            }
+            colorHistoryListBox.Items.Add(new ListBoxItem { Content = colorPanel });
+
+            int penaltyPoints = CalculatePenaltyPoints(inputColor); // Berekening van strafpunten
+            score -= penaltyPoints; // Aftrekken van strafpunten van de score
+            labelScore.Content = $"Score: {score}"; // Bijwerken van de score
+            
+            if (inputColor.SequenceEqual(secretCode))
+            {
+                timer.Stop();
+                MessageBoxResult result = MessageBox.Show($"Proficiat, je hebt de code gekraakt in {guessAttempts} pogingen. Wil je opnieuw spelen?",
+                    "Gewonnen", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                if (result == MessageBoxResult.Yes)
+                {
+                    ResetGame();
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+                return;
+            }
+            
+            for (int i = 0; i < 4; i++)
+            {
+                Label checkColors = null;
+
+                switch (i)
+                {
+                    case 0:
+                        checkColors = labelColorOne;
+                        break;
+                    case 1:
+                        checkColors = labelColorTwo;
+                        break;
+                    case 2:
+                        checkColors = labelColorThree;
+                        break;
+                    case 3:
+                        checkColors = labelColorFour;
+                        break;
+                }
+            }            
+        }
+        private void ConfirmExit()
+        {
+            MessageBoxResult result = MessageBox.Show("Weet je zeker dat je het spel wilt beëindigen?", "Bevestig Afsluiten", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+        private void ResetGame()
+        {
+            secretCode.Clear();
+            var randomCode = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                secretCode.Add(colors[randomCode.Next(colors.Count)]);
+            }
+
+            guessAttempts = 0;
+            score = 100;
+            labelScore.Content = $"Score: {score}";
+            colorHistoryListBox.Items.Clear();
+            this.Title = $"Poging: {guessAttempts}";
+            //reset comboboxes + labels.
+            comboBoxColour1.SelectedIndex = -1;
+            comboBoxColour2.SelectedIndex = -1;
+            comboBoxColour3.SelectedIndex = -1;
+            comboBoxColour4.SelectedIndex = -1;
+
+            labelColorOne.Background = new SolidColorBrush(Colors.Transparent);
+            labelColorTwo.Background = new SolidColorBrush(Colors.Transparent);
+            labelColorThree.Background = new SolidColorBrush(Colors.Transparent);
+            labelColorFour.Background = new SolidColorBrush(Colors.Transparent);
+            StartCountDown();
+
+        }
+        private int CalculatePenaltyPoints(List<string> userGuess)
+        {
+            int penaltyPoints = 0;
+
+            for (int i = 0; i < userGuess.Count; i++)
+            {
+                if (userGuess[i] == secretCode[i])
+                {
+                    // Correct color and position
+                    penaltyPoints += 0;
+                }
+                else if (secretCode.Contains(userGuess[i]))
+                {
+                    // Correct color but wrong position
+                    penaltyPoints += 1;
+                }
+                else
+                {
+                    // Color not in the code
+                    penaltyPoints += 2;
+                }
+            }
+
+            return penaltyPoints;
+        }
+        private void UpdateTitle()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(guessAttempts);
+            this.Title = sb.ToString();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.F12)
+            {
+                ToggleDebug();
+            }
+        }
+        /// <summary>
+        /// Hier kijkt de methode of de textbox zichtbaar is of niet.
+        /// Vervolgens kan ik de textbox zichtbaar maken of verbergen met de toetsencombinatie CTRL + F12.
+        /// </summary>
+        private void ToggleDebug()
+        {
+            if (generatedCodeTextBox.Visibility == Visibility.Visible)
+            {
+                generatedCodeTextBox.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                if (secretCode is List<string> secretCodeList)
+                {
+                    generatedCodeTextBox.Text = string.Join(", ", secretCodeList);
+                    generatedCodeTextBox.Visibility = Visibility.Visible;
+                }
+            }
+        }
+    }
+}
+
+
+     
+    
